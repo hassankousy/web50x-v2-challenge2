6613b32f121f583a5f294a9b2ac19804dcce7cf6
hassankousy <hassankousy3@gmail.com>
2022-08-13 18:43:35 +0300

    update solve task

--- ip-comparison.py
+++ ip-comparison.py
@@ -15,7 +15,7 @@ ip =(input("enter your ip :"))
 n=ip.split("/")
 fulnum=n[0]
 oct=fulnum.split(".")
-mask=n[1]
+mask=int(n[1])
 length=len(fulnum)
 oct1=int(oct[0])
 oct2=int(oct[1])
@@ -24,32 +24,32 @@ oct4=int(oct[3])
 
 
 # 0<= (oct2,oct3,oct4) <=255:
+if 0<= oct1 <=255 and 0<= oct2 <=255 and 0<= oct3 <=255 and 0<= oct4 <=255 and 0<= mask <=34 :
 
+    if  0<= oct1 <=127:
+        if oct1 == 10 :
+            print('class A : private ip range')
+        elif oct1 == 127:
+            print('class A : Special ip range')
+        else:
+            print('class A : public ip range')
 
-if  0<= oct1 <=127 and 0<= oct2 <=255 and 0<= oct3 <=255 and 0<= oct4 <=255  :
-    if oct1 == 10 :
-        print('class A : private ip range')
-    elif oct1 == 127:
-        print('class A : Special ip range')
-    else:
-       print('class A : public ip range')
+    elif 128<= oct1 <=191   :
+        if oct1 == 172 and 16<= (oct2) <=31 :
+            print('class B : private ip range')
+        else:
+            print('class B : public ip range')
 
-elif 128<= oct1 <=191 and 0<= oct2 <=255 and 0<= oct3 <=255 and 0<= oct4 <=255  :
-    if oct1 == 172 and 16<= (oct2) <=31 :
-        print('class B : private ip range')
-    else:
-        print('class B : public ip range')
+    elif 192<= oct1 <=223 :
+        if oct1 == 192 and  (oct2) ==168 :
+            print('class C : private ip range')
+        else:   
+            print('class C : public ip range')
 
-elif 192<= oct1 <=223 and 0<= oct2 <=255 and 0<= oct3 <=255 and 0<= oct4 <=255 :
-    if oct1 == 192 and  (oct2) ==168 :
-        print('class C : private ip range')
-    else:   
-        print('class C : public ip range')
-
-elif 124<= oct1 <=239 and 0<= oct2 <=255 and 0<= oct3 <=255 and 0<= oct4 <=255 :
-    print('class D : special')
-elif 124<= oct1 <=239 and 0<= oct2 <=255 and 0<= oct3 <=255 and 0<= oct4 <=255 :
-    print('class E : public ip range')
+    elif 124<= oct1 <=239  :
+        print('class D : special')
+    elif 124<= oct1 <=239  :
+        print('class E : public ip range')
 
 else:
     print("error")
